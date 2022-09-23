24620da0b4ece59379946fd2081e012f07bfad6f
Miguel Angel López <miguelangel.lopez@asseco.es>
2022-09-23 13:08:05 +0200

    Pending push file MarsRoverTest.cs

--- MarsRoverTester/MarsRoverTest.cs
+++ MarsRoverTester/MarsRoverTest.cs
@@ -8,51 +8,53 @@ namespace MarsRoverTester
     [TestFixture]
     public class MarsRoverTest
     {
-        [TestCase(1,1,'N', "1 2 N" )]
-        [TestCase(1,1,'S', "1 0 S")]
-        [TestCase(1,1,'E', "2 1 E")]
-        [TestCase(1,1,'O', "0 1 O")]
-        public void MovementAhead(int iniX, int iniY, char iniOrientation, string expectedPosition)
+        [TestCase(1,1,CardinalPoint.Norte, "1 2 N" )]
+        [TestCase(1,1,CardinalPoint.Sur, "1 0 S")]
+        [TestCase(1,1,CardinalPoint.Este, "2 1 E")]
+        [TestCase(1,1,CardinalPoint.Oeste, "0 1 O")]
+        public void MovementAhead(int iniX, int iniY, CardinalPoint iniOrientation, string expectedPosition)
         {
-            Map map = new Map(2, 3);
-            ExplorerRover rover = new ExplorerRover(iniX, iniY, iniOrientation, map);
-            Assert.AreEqual(expectedPosition, rover.moveAhead());
-        }
-        
-        [TestCase(1,0,'S', "1 0 S")]
-        [TestCase(0,1,'O', "0 1 O")]
-        [TestCase(0,3,'N', "0 3 N")]
-        [TestCase(2,0,'E', "2 0 E")]
-        public void GivenTheRoverIsInTheLimitWhenMovingAheadWillStayAtSamePosition(int iniX, int iniY, char iniOrientation, string expectedPosition)
-        {
-            
-            Map map = new Map(2, 3);
-            ExplorerRover rover = new ExplorerRover(iniX, iniY, iniOrientation, map);
+            ExplorerRover rover = InitializeExplorer(iniX, iniY, iniOrientation);
             Assert.AreEqual(expectedPosition, rover.moveAhead());
         }
 
-        [TestCase(1, 1, 'N', "1 0 N")]
-        [TestCase(1, 1, 'S', "1 2 S")]
-        [TestCase(1, 1, 'E', "0 1 E")]
-        [TestCase(1, 1, 'O', "2 1 O")]
-        public void MovementRear(int iniX, int iniY, char iniOrientation, string expectedPosition)
+        [TestCase(1,0,CardinalPoint.Sur, "1 0 S")]
+        [TestCase(0,1,CardinalPoint.Oeste, "0 1 O")]
+        [TestCase(0,3,CardinalPoint.Norte, "0 3 N")]
+        [TestCase(2,0,CardinalPoint.Este, "2 0 E")]
+        public void GivenTheRoverIsInTheLimitWhenMovingAheadWillStayAtSamePosition(int iniX, int iniY, CardinalPoint iniOrientation, string expectedPosition)
         {
-            Map map = new Map(2, 3);
+            ExplorerRover rover = InitializeExplorer(iniX, iniY, iniOrientation);
+            Assert.AreEqual(expectedPosition, rover.moveAhead());
+        }
 
-
-            ExplorerRover rover = new ExplorerRover(iniX, iniY, iniOrientation, map);
+        [TestCase(1, 1, CardinalPoint.Norte, "1 0 N")]
+        [TestCase(1, 1, CardinalPoint.Sur, "1 2 S")]
+        [TestCase(1, 1, CardinalPoint.Este, "0 1 E")]
+        [TestCase(1, 1, CardinalPoint.Oeste, "2 1 O")]
+        public void MovementRear(int iniX, int iniY, CardinalPoint iniOrientation, string expectedPosition)
+        {
+            ExplorerRover rover = InitializeExplorer(iniX, iniY, iniOrientation);
             Assert.AreEqual(expectedPosition, rover.moveRear());
         }
 
-        [TestCase(1, 0, 'N', "1 0 N")]
-        [TestCase(0, 1, 'E', "0 1 E")]
-        [TestCase(0, 3, 'S', "0 3 S")]
-        [TestCase(2, 0, 'O', "2 0 O")]
-        public void GivenTheRoverIsInTheLimitWhenMovingRearWillStayAtSamePosition(int iniX, int iniY, char iniOrientation, string expectedPosition)
+        [TestCase(1, 0, CardinalPoint.Norte, "1 0 N")]
+        [TestCase(0, 1, CardinalPoint.Este, "0 1 E")]
+        [TestCase(0, 3, CardinalPoint.Sur, "0 3 S")]
+        [TestCase(2, 0, CardinalPoint.Oeste, "2 0 O")]
+        public void GivenTheRoverIsInTheLimitWhenMovingRearWillStayAtSamePosition(int iniX, int iniY, CardinalPoint iniOrientation, string expectedPosition)
         {
-            Map map = new Map(2, 3);
-            ExplorerRover rover = new ExplorerRover(iniX, iniY, iniOrientation, map);
+            ExplorerRover rover = InitializeExplorer(iniX, iniY, iniOrientation);
             Assert.AreEqual(expectedPosition, rover.moveRear());
+        }
+
+
+        private static ExplorerRover InitializeExplorer(int iniX, int iniY, CardinalPoint iniOrientation)
+        {
+            Point point = new Point(iniX, iniY);
+            Map map = new Map(2, 3);
+            ExplorerRover rover = new ExplorerRover(point, iniOrientation, map);
+            return rover;
         }
     }
 }
